7996596a8a07a0391ae3010dcb7bbcd17bff15fe
anarcher <anarcher@gmail.com>
2016-04-25 12:59:09 +0900

    Add collection_id_encryption_key option

--- start.sh
+++ start.sh
@@ -13,9 +13,11 @@ DB_PASSWORD=${DB_PASSWORD:-password}
 TOP_DOMAINS=${TOP_DOMAINS:-localhost,localbox.info}
 DEFALTID_ENCRYPTION_KEY=${DEFALTID_ENCRYPTION_KEY:-0123456789abcdef}
 ACCOUNTID_ENCRYPTION_KEY=${ACCOUNTID_ENCRYPTION_KEY:-0123456789abcdef}
+COLLECTION_ID_ENCRYPTION_KEY=${COLLECTION_ID_ENCRYPTION_KEY:-0123456789abcdef}
 
 cp hackpad/etherpad/etc/etherpad.local.properties.tmpl hackpad/etherpad/etc/etherpad.local.properties
 
+sed -i.bak s/__collection_id_encryption_key__/$COLLECTION_ID_ENCRYPTION_KEY/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak s/__account_id_encryption_key__/$ACCOUNTID_ENCRYPTION_KEY/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak s/__default_id_encryption_key__/$DEFALTID_ENCRYPTION_KEY/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak s/__email_addresses_with_admin_access__/$ADMIN_EMAILS/g hackpad/etherpad/etc/etherpad.local.properties
